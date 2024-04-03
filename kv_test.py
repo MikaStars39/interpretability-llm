bb68e4c4fb833c2c78b0deb4886ecb29d173d26a
MikaStars39 <meloxdmy@qq.com>
2024-04-03 16:23:27 +0800

    new kv

--- kv_test.py
+++ kv_test.py
@@ -4,54 +4,46 @@ import argparse
 
 from tqdm import tqdm
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer
 from datasets import load_dataset
-from transformers import AutoModelForCausalLM
+
+from transformers import AutoTokenizer
 
 from src.modeling_gptneo import GPTNeoForCausalLM
-from src.utils import draw_rv_figure, draw_attention_maps
-
-kv_prompt = "I will give you some keys and values. Then I ask you a key, you respones me with its value. "
-example = ""
-
-def test_dataset():
-    dataset = load_dataset("json", data_files="kv_test/kv_pairs_100_100.json", split="train")
-    print(len(next(iter(dataset))["0"][0]))
+from kv_test.kv_generation import generate_kv
 
 
 @torch.no_grad()
 def test_kv(args):
-    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path).to("cuda")
+    model = GPTNeoForCausalLM.from_pretrained(args.model_name_or_path).to("cuda")
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
-    dataset = load_dataset("json", data_files=args.data_name_or_path, split="train")
+    dataset = generate_kv()
+    acc = 0
+    perplexity = 0
 
-    length = len(tokenizer.encode(next(iter(dataset))["0"][0]))
-    total_num = args.context_len // length - 4
+    for ids, batch in tqdm(enumerate(dataset)):
+        answer, query = batch
+        inputs = tokenizer(query, return_tensors="pt").to("cuda")
+        outputs = model.generate(
+            inputs["input_ids"], 
+            max_length=inputs["input_ids"].size(1) + 10, 
+            num_return_sequences=1, 
+            pad_token_id=tokenizer.eos_token_id
+            )
 
-    acc = [ 0 for _ in range(0, total_num, args.span)]
+        input_ids = inputs["input_ids"] 
+        outputs_p = model(input_ids[:, :input_ids.size(1)-1], labels=input_ids[:, 1:input_ids.size(1)])
+        loss = outputs_p.loss
+        perplexity += loss.item() / 49
+         
+        outputs = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        if answer == outputs[len(query):len(query)+8]:
+            acc = acc + 1
+    acc = acc / len(dataset)
+    print(acc, perplexity)
 
-    dataloader = DataLoader(dataset, batch_size=args.batch_size)
 
-    for ids, query in tqdm(enumerate(dataloader)):
-        query = dataset[f"{ids}"][0]
-        input_text = kv_prompt
-        for each_query_id in range(total_num):
-            each_query = query[each_query_id]
-            input_text += each_query + "; "
-        for position in range(0, total_num, args.span):
-            prompt = "Question: the key is " + query[position][5:21] + ", so the value is"
-            input_text_now = input_text + prompt
-
-            inputs = tokenizer(input_text_now, return_tensors="pt").to("cuda")
-            outputs = model.generate(inputs["input_ids"], max_length=inputs["input_ids"].size(1) + 16, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-            outputs = tokenizer.decode(outputs[0], skip_special_tokens=True)
-            if outputs[len(input_text_now)+1:len(input_text_now)+17] == query[position][-16:]:
-                acc[position // args.span] += 1
-        print(acc)
     
-    acc = torch.tensor(acc) / len(dataset)
-    print(acc)
 
 
 if __name__ == "__main__":
@@ -63,8 +55,9 @@ if __name__ == "__main__":
     parser.add_argument("--context_len", type=int, default=1024)
     parser.add_argument("--epoch", type=int, default=100)
     parser.add_argument("--span", type=int, default=5)
+    parser.add_argument("--len", type=int, default=16) 
     parser.add_argument("--batch_size", type=int, default=1)
     args = parser.parse_args()
 
     test_kv(args)
-    # test_dataset()
+    
