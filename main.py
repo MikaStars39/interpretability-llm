0075bae938c2b3a60cad74fad202476302f76f14
MikaStars39 <meloxdmy@qq.com>
2024-04-14 20:52:47 +0800

    circuits

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import torch
 from transformers import AutoTokenizer
-from src.modeling_qwen2 import Qwen2ForCausalLM
+from src.modeling_llama import LlamaForCausalLM
 
-model = Qwen2ForCausalLM.from_pretrained("/home/qingyu_yin/model/Qwen1.5-1.8B").to("cuda")
-tokenizer = AutoTokenizer.from_pretrained("/home/qingyu_yin/model/Qwen1.5-1.8B")
+model = LlamaForCausalLM.from_pretrained(
+    "/home/qingyu_yin/model/llama-2-7b-hf",
+    device_map="auto",
+    torch_dtype=torch.float16,
+    )
+tokenizer = AutoTokenizer.from_pretrained("/home/qingyu_yin/model/llama-2-7b-hf")
 
-prompt = "Hey, are you conscious? Can you talk to me?"
-inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+prompt = "Large Language Models (LLMs) are deep learning models that can efficiently process and understand \
+    natural language text. One challenge in the application of LLMs is their enormous number of parameters. "
+inputs = tokenizer(prompt, return_tensors="pt").to("cuda").to(torch.float16)
 inputs["labels"] = inputs["input_ids"]
-
-model.model.skip_list = [21, 22, 23, 24]
-model.model.skip_from = "linear"
-
 outputs = model.forward(**inputs)
+base_loss = outputs.loss
 
-print(outputs.loss)
+for skip in range(1, 32):
+    from src.functions import test_qa
+    
+    model.model.skip_list = [skip]
+    model.model.skip_from = "direct"
+
+    outputs = model.forward(**inputs)
+
+    print(outputs.loss - base_loss)
